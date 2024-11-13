39ac1695310939bf2dcae70addc2b67040226545
Alberto Pontiery <137962796+alry03@users.noreply.github.com>
2024-11-12 23:36:36 -0300

    Add files via upload

---
+++ retangulo.py
@@ -0,0 +1,13 @@
+import cv2
+
+# Path do arquivo jpg
+img = cv2.imread(r'C:\Users\alberto\Pictures\parisjpg.jpg')
+
+# Desenha o retângulo
+cv2.rectangle(img, (30, 30), (30 + 50, 30 + 100), (0, 0, 255), 5)
+
+# Mostra a Imagem na tela
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
