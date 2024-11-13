4203a3a69c22f99d8c37f2cffca1f78dc3acef07
Alberto Pontiery <137962796+alry03@users.noreply.github.com>
2024-11-12 23:57:06 -0300

    Redimensionando o retângulo

--- retangulo.py
+++ retangulo.py
@@ -4,10 +4,11 @@ import cv2
 img = cv2.imread(r'C:\Users\alberto\Pictures\parisjpg.jpg')
 
 # Desenha o retângulo
-cv2.rectangle(img, (30, 30), (30 + 50, 30 + 100), (0, 0, 255), 5)
+cv2.rectangle(img, (30, 30), (100 + 150, 100 + 250), (0, 0, 255), 5)
 
 # Mostra a Imagem na tela
 cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
 
